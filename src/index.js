e35760b34a5cc90630a6959c779e90055e28fb3c
Qhristen <odeyxtian@gmail.com>
2020-05-22 13:35:28 +0100

    chore: setup serviceWorker and scss

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/scss/main.scss';
 import App from './App';
+import * as serviceWorker from "./serviceWorker";
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
+serviceWorker.register();
